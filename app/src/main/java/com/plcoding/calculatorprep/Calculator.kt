7b1e3f72af74bc2af2f42b308b3d6a323122685b
Robertcsollei <robertcsollei@gmail.com>
2023-05-28 21:04:33 +0200

    adding calculator keyboard

--- app/src/main/java/com/plcoding/calculatorprep/Calculator.kt
+++ app/src/main/java/com/plcoding/calculatorprep/Calculator.kt
@@ -1,25 +1,23 @@
 package com.plcoding.calculatorprep
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.plcoding.calculatorprep.components.CalculatorButton
 import com.plcoding.calculatorprep.components.CalculatorDisplay
+import com.plcoding.calculatorprep.components.CalculatorKeyboard
 
 @Composable
 fun Calculator(
     state: CalculatorState,
     modifier: Modifier = Modifier,
     buttonSpacing: Dp = 8.dp,
+    onAction: (CalculatorAction) -> Unit
 ) {
 
 
@@ -34,14 +32,7 @@ fun Calculator(
             verticalArrangement = Arrangement.spacedBy(buttonSpacing)
         ) {
             CalculatorDisplay(state.numbers, state.operators)
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
-            ) {
-                CalculatorButton(symbol = "AC", modifier = Modifier.background(LightGray)) {
-
-                }
-            }
+            CalculatorKeyboard(buttonSpacing, onAction)
         }
     }
 }
--- app/src/main/java/com/plcoding/calculatorprep/MainActivity.kt
+++ app/src/main/java/com/plcoding/calculatorprep/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.plcoding.calculatorprep.ui.theme.CalculatorPrepTheme
@@ -17,7 +18,12 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<CalculatorViewModel>();
                 val state = viewModel.state;
                 val buttonSpacing = 8.dp;
-                
+                Calculator(
+                    state,
+                    Modifier,
+                    buttonSpacing,
+                    onAction = { action -> println(action) }
+                )
 
             }
         }
